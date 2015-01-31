97e47799c96932bcfb79bfb9ea9430a2d8fad24e
Kevin Ho <keho98@gmail.com>
2015-01-31 12:53:54 -0800

    Initial Commit

---
+++ Homepwner/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Homepwner
+//
+//  Created by Kevin Ho on 1/31/15.
+//  Copyright (c) 2015 kho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Homepwner/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Homepwner
+//
+//  Created by Kevin Ho on 1/31/15.
+//  Copyright (c) 2015 kho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
