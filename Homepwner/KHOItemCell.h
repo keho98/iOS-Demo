fe228e8b0aee2ab7c22985bc1808e5f59fbe96f4
Kevin Ho <keho98@gmail.com>
2015-02-16 16:14:17 -0800

    Starts UITableViewCell subclass

---
+++ Homepwner/KHOItemCell.h
@@ -0,0 +1,13 @@
+//
+//  KHOItemCell.h
+//  Homepwner
+//
+//  Created by Kevin Ho on 2/16/15.
+//  Copyright (c) 2015 kho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KHOItemCell : UITableViewCell
+
+@end
---
+++ Homepwner/KHOItemCell.m
@@ -0,0 +1,23 @@
+//
+//  KHOItemCell.m
+//  Homepwner
+//
+//  Created by Kevin Ho on 2/16/15.
+//  Copyright (c) 2015 kho. All rights reserved.
+//
+
+#import "KHOItemCell.h"
+
+@implementation KHOItemCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
